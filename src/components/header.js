fb92600ff41988681ccdd89b1771b843af80e612
trustiva <trustiva420@gmail.com>
2025-08-14 05:26:01 +0330

    Change logo text from hypefy to catdog

---
+++ src/components/header.js
@@ -0,0 +1,5 @@
+@@ .. @@
+       <div class="flex items-center">
+-        <span class="text-2xl font-bold text-gray-900">hypefy</span>
++        <span class="text-2xl font-bold text-gray-900">catdog</span>
+       </div>
